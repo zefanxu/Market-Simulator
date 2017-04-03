adbf212e4a5a4e6dfcb4051944a07564ef7fe0dc
Zefan Xu <zxu@fountainhead>
2017-04-03 10:09:31 -0500

    rearrange

--- boe_session.cpp
+++ boe_session.cpp
@@ -44,6 +44,17 @@ void boe_session::handle_packet(char * packet, size_t len){
   }
 }
 
+void boe_session::handle_client_heartbeat(MsgHeader* hdr, size_t len){
+  last_recv_heartbeat = clock();
+}
+
+void boe_session::handle_login_request(MsgHeader* hdr, size_t len){
+  LoginRequest * req = reinterpret_cast<LoginRequest*>(hdr);
+  if (state != session_state::not_logged_in) constructLoginResponse(LoginResponseStatus::SessionInUse, req);
+  else constructLoginResponse(LoginResponseStatus::Accepted, req);
+  state = session_state::logged_in;
+}
+
 void boe_session::modifyOrder(MsgHeader * msg, size_t len){
   ModifyOrder * mo = reinterpret_cast<ModifyOrder*>(msg);
   string t = mo->orig_token._str_();
@@ -53,75 +64,6 @@ void boe_session::modifyOrder(MsgHeader * msg, size_t len){
   }
   Boe_ModifyOrderReq modify_req = Boe_ModifyOrderReq(mo);
   pending_modify.push_back(modify_req);
-}
-
-void boe_session::constructModifyRejected(Token t, Reason r){
-  ModifyRejected mr;
-  mr.transaction_time = get_timestamp();
-  mr.token = t;
-  mr.reason = static_cast<char>(r);
-  strcpy(mr.text, to_string(r));
-  mr.num_bitfields = 0;
-  auto packet = vector<char>(reinterpret_cast<char*>(&mr), reinterpret_cast<char*>(&mr)+sizeof(mr));
-  pending_out_messages.push_back(packet);
-}
-
-void boe_session::modify_logic(){
-  vector<string> done_tokens;
-  for (const auto & mo: pending_modify){
-    if (active_orders.find(mo.orig_token._str_()) == active_orders.end())
-      continue;
-    auto & target_order = active_orders[mo.orig_token._str_()];
-
-    if (target_order.executed_qty >= mo.qty){
-      done_tokens.push_back(target_order.token._str_());
-      target_order.remaining_qty = 0;
-    }
-    else target_order.remaining_qty = mo.qty - target_order.executed_qty;
-    target_order.side = mo.side;
-    target_order.price = mo.price;
-    target_order.token = mo.token;
-    strncpy(target_order.firm, mo.clearing_firm, 4);
-    //change the key in the map
-    swap(active_orders[mo.token._str_()], active_orders[mo.orig_token._str_()]);
-    active_orders.erase(mo.orig_token._str_());
-    constructOrderModified(active_orders[mo.token._str_()]);
-  }
-  for (const auto & each_token : done_tokens)
-    active_orders.erase(each_token);
-}
-
-void boe_session::constructOrderModified(const Boe_Order & bo){
-  OrderModified om;
-  //price, OrderQty, LeavesQty
-  om.bitfield[0] = 4;
-  om.bitfield[1] = 0;
-  om.bitfield[2] = 64;
-  om.bitfield[3] = 0;
-  om.bitfield[4] = 2;
-  om.price = bo.price;
-  om.leaves_qty = bo.remaining_qty;
-  om.qty = bo.remaining_qty + bo.executed_qty;
-  om.transaction_time = get_timestamp();
-  om.order_id = bo.orderID;
-  om.token = bo.token;
-  auto packet = vector<char>(reinterpret_cast<char*>(&om), reinterpret_cast<char*>(&om)+sizeof(om));
-  pending_out_messages.push_back(packet);
-}
-
-void boe_session::cancel_logic(){
-  vector<string> done_tokens;
-  for (const auto & co : pending_cancel){
-    if (active_orders.find(co.token._str_()) == active_orders.end()){
-      constructCancelRejected(co.token, Reason::TooLateToCancel);
-      continue;
-    }
-    done_tokens.push_back(co.token._str_());
-    constructOrderCanceled(co.token);
-  }
-  pending_cancel.clear();
-  for (const auto & each_token : done_tokens)
-    active_orders.erase(each_token);
 }
 
 void boe_session::cancelOrder(MsgHeader * msg, size_t len){
@@ -136,28 +78,6 @@ void boe_session::cancelOrder(MsgHeader * msg, size_t len){
   else constructCancelRejected(co->orig_token, Reason::ClordidDoesntMatchAKnownOrder);
 }
 
-void boe_session::constructOrderCanceled(Token t){
-  OrderCanceled oc;
-  oc.transaction_time = get_timestamp();
-  oc.token = t;
-  oc.reason = static_cast<char>(Reason::UserRequested);
-  memset(oc.bitfield, 0, sizeof(oc.bitfield));
-  oc.bitfield[4] = 2;
-  auto packet = vector<char>(reinterpret_cast<char*>(&oc), reinterpret_cast<char*>(&oc)+sizeof(oc));
-  pending_out_messages.push_back(packet);
-}
-
-void boe_session::constructCancelRejected(Token t, Reason r){
-  CancelRejected cr;
-  cr.transaction_time = get_timestamp();
-  cr.token = t;
-  cr.reason = static_cast<char>(r);
-  cr.num_bitfields = 0;
-  strcpy(cr.text, to_string(r));
-  auto packet = vector<char>(reinterpret_cast<char*>(&cr), reinterpret_cast<char*>(&cr)+sizeof(cr));
-  pending_out_messages.push_back(packet);
-}
-
 void boe_session::enterOrder(MsgHeader * hdr, size_t len){
   NewOrder * no = reinterpret_cast<NewOrder*>(hdr);
   string t = no->token._str_();
@@ -169,78 +89,6 @@ void boe_session::enterOrder(MsgHeader * hdr, size_t len){
     Boe_Order new_order = Boe_Order(no);
     active_orders[t] = new_order;
     constructOrderAccpeted(new_order);
-  }
-}
-
-void boe_session::constructOrderRejected(NewOrder * no){
-  OrderRejected oj;
-  oj.token = no->token;
-  oj.reason = 'D';
-  strcpy(oj.text, "Duplicate CIOrdID");
-  auto packet = vector<char>(reinterpret_cast<char*>(&oj), reinterpret_cast<char*>(&oj)+sizeof(oj));
-  pending_out_messages.push_back(packet);
-}
-
-void boe_session::constructOrderAccpeted(Boe_Order & new_order){
-  OrderAck ack;
-  ack.seq_num = ++seq_num;
-  ack.transaction_time = get_timestamp();
-  ack.order_id = new_order.orderID;
-  ack.token = new_order.token;
-  ack.num_bitfields = 5;
-  memset(ack.bitfield, 0, sizeof(ack.bitfield));
-  ack.bitfield[4] = 2;
-  ack.leaves_qty = new_order.remaining_qty;
-  auto packet = vector<char>(reinterpret_cast<char*>(&ack), reinterpret_cast<char*>(&ack)+sizeof(ack));
-  pending_out_messages.push_back(packet);
-}
-
-void boe_session::handle_client_heartbeat(MsgHeader* hdr, size_t len){
-  last_recv_heartbeat = clock();
-}
-
-void boe_session::handle_login_request(MsgHeader* hdr, size_t len){
-  LoginRequest * req = reinterpret_cast<LoginRequest*>(hdr);
-  if (state != session_state::not_logged_in) constructLoginResponse(LoginResponseStatus::SessionInUse, req);
-  else constructLoginResponse(LoginResponseStatus::Accepted, req);
-  state = session_state::logged_in;
-}
-
-void boe_session::constructLoginResponse(LoginResponseStatus status, LoginRequest * req){
-  char * buf = (char*)calloc(300, sizeof(char));
-  LoginResponse lr;
-  lr.status = static_cast<uint8_t>(status);
-  memset(lr.text, 0, sizeof(lr.text));
-  lr.last_received_seq_num = 0;
-  lr.no_unspecified_unit_replay = req->replay.no_unspecified_unit_replay;
-  lr.number_of_units = 1;
-  auto packet_begin = (LoginResponse*)buf;
-  *(packet_begin) = lr;
-  packet_begin->unit[0] = UnitSequence();
-  packet_begin->unit[0].number = 1;
-  packet_begin->unit[0].seq_num = 0;
-  //echo back the param groups
-  memcpy(&(packet_begin->unit[1]), &(req->order_ack), (sizeof(ReturnBitfieldParamGroup)*4+20));
-  size_t packet_size = sizeof(lr) + sizeof(UnitSequence) + sizeof(ReturnBitfieldParamGroup) * 4 + 20;
-  packet_begin->length = packet_size-2;
-  auto packet = vector<char>(buf, buf + packet_size);
-  pending_out_messages.push_back(packet);
-  free(buf);
-}
-
-uint64_t boe_session::get_timestamp(){
-  chrono::milliseconds ms = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
-  return ms.count() * 1000;
-}
-
-void boe_session::heartbeat_logic(){
-  double second = difftime(time(NULL), last_send_heartbeat);
-  if (state == session_state::not_logged_in) return;
-  if (second >= 1){
-    last_send_heartbeat = time(NULL);
-    ServerHeartbeat h;
-    auto packet = vector<char>(reinterpret_cast<const char*>(&h), reinterpret_cast<const char*>(&h) + sizeof(h));
-    pending_out_messages.push_back(packet);
   }
 }
 
@@ -269,6 +117,153 @@ void boe_session::execution_logic(){
     active_orders.erase(each_token);
 }
 
+void boe_session::modify_logic(){
+  vector<string> done_tokens;
+  for (const auto & mo: pending_modify){
+    if (active_orders.find(mo.orig_token._str_()) == active_orders.end())
+      continue;
+    auto & target_order = active_orders[mo.orig_token._str_()];
+
+    if (target_order.executed_qty >= mo.qty){
+      done_tokens.push_back(target_order.token._str_());
+      target_order.remaining_qty = 0;
+    }
+    else target_order.remaining_qty = mo.qty - target_order.executed_qty;
+    target_order.side = mo.side;
+    target_order.price = mo.price;
+    target_order.token = mo.token;
+    strncpy(target_order.firm, mo.clearing_firm, 4);
+    //change the key in the map
+    swap(active_orders[mo.token._str_()], active_orders[mo.orig_token._str_()]);
+    active_orders.erase(mo.orig_token._str_());
+    constructOrderModified(active_orders[mo.token._str_()]);
+  }
+  for (const auto & each_token : done_tokens)
+    active_orders.erase(each_token);
+}
+
+void boe_session::cancel_logic(){
+  vector<string> done_tokens;
+  for (const auto & co : pending_cancel){
+    if (active_orders.find(co.token._str_()) == active_orders.end()){
+      constructCancelRejected(co.token, Reason::TooLateToCancel);
+      continue;
+    }
+    done_tokens.push_back(co.token._str_());
+    constructOrderCanceled(co.token);
+  }
+  pending_cancel.clear();
+  for (const auto & each_token : done_tokens)
+  active_orders.erase(each_token);
+}
+
+void boe_session::heartbeat_logic(){
+  double second = difftime(time(NULL), last_send_heartbeat);
+  if (state == session_state::not_logged_in) return;
+  if (second >= 1){
+    last_send_heartbeat = time(NULL);
+    ServerHeartbeat h;
+    auto packet = vector<char>(reinterpret_cast<const char*>(&h), reinterpret_cast<const char*>(&h) + sizeof(h));
+    pending_out_messages.push_back(packet);
+  }
+}
+
+void boe_session::constructModifyRejected(Token t, Reason r){
+  ModifyRejected mr;
+  mr.transaction_time = get_timestamp();
+  mr.token = t;
+  mr.reason = static_cast<char>(r);
+  strcpy(mr.text, to_string(r));
+  mr.num_bitfields = 0;
+  auto packet = vector<char>(reinterpret_cast<char*>(&mr), reinterpret_cast<char*>(&mr)+sizeof(mr));
+  pending_out_messages.push_back(packet);
+}
+
+void boe_session::constructOrderModified(const Boe_Order & bo){
+  OrderModified om;
+  //price, OrderQty, LeavesQty
+  om.bitfield[0] = 4;
+  om.bitfield[1] = 0;
+  om.bitfield[2] = 64;
+  om.bitfield[3] = 0;
+  om.bitfield[4] = 2;
+  om.price = bo.price;
+  om.leaves_qty = bo.remaining_qty;
+  om.qty = bo.remaining_qty + bo.executed_qty;
+  om.transaction_time = get_timestamp();
+  om.order_id = bo.orderID;
+  om.token = bo.token;
+  auto packet = vector<char>(reinterpret_cast<char*>(&om), reinterpret_cast<char*>(&om)+sizeof(om));
+  pending_out_messages.push_back(packet);
+}
+
+void boe_session::constructOrderCanceled(Token t){
+  OrderCanceled oc;
+  oc.transaction_time = get_timestamp();
+  oc.token = t;
+  oc.reason = static_cast<char>(Reason::UserRequested);
+  memset(oc.bitfield, 0, sizeof(oc.bitfield));
+  oc.bitfield[4] = 2;
+  auto packet = vector<char>(reinterpret_cast<char*>(&oc), reinterpret_cast<char*>(&oc)+sizeof(oc));
+  pending_out_messages.push_back(packet);
+}
+
+void boe_session::constructCancelRejected(Token t, Reason r){
+  CancelRejected cr;
+  cr.transaction_time = get_timestamp();
+  cr.token = t;
+  cr.reason = static_cast<char>(r);
+  cr.num_bitfields = 0;
+  strcpy(cr.text, to_string(r));
+  auto packet = vector<char>(reinterpret_cast<char*>(&cr), reinterpret_cast<char*>(&cr)+sizeof(cr));
+  pending_out_messages.push_back(packet);
+}
+
+void boe_session::constructOrderRejected(NewOrder * no){
+  OrderRejected oj;
+  oj.token = no->token;
+  oj.reason = 'D';
+  strcpy(oj.text, "Duplicate CIOrdID");
+  auto packet = vector<char>(reinterpret_cast<char*>(&oj), reinterpret_cast<char*>(&oj)+sizeof(oj));
+  pending_out_messages.push_back(packet);
+}
+
+void boe_session::constructOrderAccpeted(Boe_Order & new_order){
+  OrderAck ack;
+  ack.seq_num = ++seq_num;
+  ack.transaction_time = get_timestamp();
+  ack.order_id = new_order.orderID;
+  ack.token = new_order.token;
+  ack.num_bitfields = 5;
+  memset(ack.bitfield, 0, sizeof(ack.bitfield));
+  ack.bitfield[4] = 2;
+  ack.leaves_qty = new_order.remaining_qty;
+  auto packet = vector<char>(reinterpret_cast<char*>(&ack), reinterpret_cast<char*>(&ack)+sizeof(ack));
+  pending_out_messages.push_back(packet);
+}
+
+void boe_session::constructLoginResponse(LoginResponseStatus status, LoginRequest * req){
+  char * buf = (char*)calloc(300, sizeof(char));
+  LoginResponse lr;
+  lr.status = static_cast<uint8_t>(status);
+  memset(lr.text, 0, sizeof(lr.text));
+  lr.last_received_seq_num = 0;
+  lr.no_unspecified_unit_replay = req->replay.no_unspecified_unit_replay;
+  lr.number_of_units = 1;
+  auto packet_begin = (LoginResponse*)buf;
+  *(packet_begin) = lr;
+  packet_begin->unit[0] = UnitSequence();
+  packet_begin->unit[0].number = 1;
+  packet_begin->unit[0].seq_num = 0;
+  //echo back the param groups
+  memcpy(&(packet_begin->unit[1]), &(req->order_ack), (sizeof(ReturnBitfieldParamGroup)*4+20));
+  size_t packet_size = sizeof(lr) + sizeof(UnitSequence) + sizeof(ReturnBitfieldParamGroup) * 4 + 20;
+  packet_begin->length = packet_size-2;
+  auto packet = vector<char>(buf, buf + packet_size);
+  pending_out_messages.push_back(packet);
+  free(buf);
+}
+
 void boe_session::constructOrderExecuted(Boe_Order & curr_order){
   OrderExecution oe;
   oe.seq_num = ++seq_num;
@@ -287,4 +282,9 @@ void boe_session::constructOrderExecuted(Boe_Order & curr_order){
   strncpy(oe.contra_broker, "BATS", 4);
   auto packet = vector<char>(reinterpret_cast<const char*>(&oe), reinterpret_cast<const char*>(&oe) + sizeof(oe));
   pending_out_messages.push_back(packet);
+}
+
+uint64_t boe_session::get_timestamp(){
+  chrono::milliseconds ms = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
+  return ms.count() * 1000;
 }
