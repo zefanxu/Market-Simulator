9a7287e711a4b5b2b6f5f2e9eb0c885cf86eec12
Zefan Xu <zxu@ld-dev10.fountainhead>
2017-04-10 11:07:14 -0500

    add testing script

--- run.py
+++ run.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 import subprocess, time
-text1 = 'om send_order B 100 SPY.ARCX 234.56 BATS'
-text2 = 'om cancel 9100'
+text1 = 'om send_order B 90000 SPY.ARCX 234.56 BATS\n'
+text2 = 'om cancel 9100\n'
 evtsim_proc = subprocess.Popen(
-    './evtsim -boeport 33333')
+    ['./evtsim','-b', '11035'])
+evts_proc = subprocess.Popen(
+    ['../evts/evts', '-c', 'evts.config'])
+time.sleep(2)
 admin_proc = subprocess.Popen(
-    '/evt/apps/infra/prod/bin/admin localhost:12011',stdout=subprocess.PIPE,
+    ['/evt/apps/infra/prod/bin/admin', 'localhost:12014'],stdout=subprocess.PIPE,
     stdin=subprocess.PIPE)
 time.sleep(3)
+if (evtsim_proc.poll() != None or admin_proc.poll() != None or evts_proc.poll() != None):
+    print "Fail to run evts or evtsim or admin console"
+    exit()
+
 admin_proc.stdin.write(text1)
 time.sleep(5)
 admin_proc.stdin.write(text2)
 time.sleep(2)
-admin_proc.stdin.close()
-result = admin_proc.stdout.read()
-#print result
-admin_proc.terminate()
-evtsim_proc.terminate()
+
+
+
+admin_proc.kill()
+evtsim_proc.kill()
+evts_proc.kill()
+
+#print admin_proc.stdout.read()
